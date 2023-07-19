fc03ba222f9c08d7c4172954019abb14484f7dbb
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2023-07-19 09:44:01 +0000

    change

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig :{
+    apiKey: "AIzaSyBL9XFjo_YZnr4j9XhJC7heLgMRi0AT7kM",
+    authDomain: "jour-date.firebaseapp.com",
+    projectId: "jour-date",
+    storageBucket: "jour-date.appspot.com",
+    messagingSenderId: "32671199013",
+    appId: "1:32671199013:web:6a223c487f89b64887b4cc",
+    measurementId: "G-0GWVZNZWB2"
+  }
+}
